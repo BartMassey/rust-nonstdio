934de65e8c85eb8e880a4a1d58f737eb18738db6
Bart Massey <bart.massey@gmail.com>
2021-10-31 15:27:33 -0700

    updated fizzbuzz example to use stdout_with_nbuf()

--- examples/fizzbuzz.rs
+++ examples/fizzbuzz.rs
@@ -6,7 +6,7 @@ fn main() {
         .unwrap_or_else(|| "10000000".to_string())
         .parse::<usize>()
         .unwrap();
-    let mut stdout = nonstdio::stdout().lock();
+    let mut stdout = nonstdio::stdout_with_nbuf(32 * 1024).lock();
     for i in 1..=n {
         match (i % 3 == 0, i % 5 == 0) {
             (false, false) => nwriteln!(stdout, "{}", i),
